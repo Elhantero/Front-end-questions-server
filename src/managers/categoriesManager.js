fd02a62fa6ecd0dfa7495f09947519514042cb63
arturfv <artur.fesenko@auto.ria.com>
2024-09-08 14:40:21 +0300

    upd

--- src/managers/categoriesManager.js
+++ src/managers/categoriesManager.js
@@ -6,8 +6,10 @@ export const getAllCategories = (req, res) => {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": allowedCORSURL,
     })
+    // поки відключу 11 і 13, тайпскріпт і ву
     const query = `select c.categoryId, c.categoryName, ct.nameUK as name from  FrontEndDB.categories as c
-                        join FrontEndDB.categoriesTranslate as ct on c.categoryId = ct.categoryId;`;
+                        join FrontEndDB.categoriesTranslate as ct on c.categoryId = ct.categoryId
+                        where c.categoryId not in (11, 13);`;
     dbQuery(query)
         .then(results => res.send(results))
         .catch(err => res.send(err, 'db getAllCategories error'));
